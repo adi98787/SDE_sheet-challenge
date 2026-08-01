ceb8d338f0262da2cf7351c001fe8d93ae35cf50
Aditya kumar <kaditya2863@gmail.com>
2026-08-02 00:50:55 +0530

    Time: 0 ms (100%), Space: 8.3 MB (11.11%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,26 +1,34 @@
 class Solution {
-public:
+public:  
+    // int fib(int n, vector<int>&dp)
+    // {
+    //     // base case
+    //     if(n<=1)
+    //     return n;
 
+    //     // recompuation remove
 
-     
-    int fib(int n, vector<int>&dp)
+    //     if(dp[n]!=-1)
+    //     return dp[n];
+
+    //     return dp[n]=fib(n-1,dp)+fib(n-2,dp);
+
+    // }
+    int fib(int n) 
     {
-        // base case
+
         if(n<=1)
         return n;
 
-        // recompuation remove
-
-        if(dp[n]!=-1)
-        return dp[n];
-
-        return dp[n]=fib(n-1,dp)+fib(n-2,dp);
-
-    }
-
-
-    int fib(int n) {
         vector<int>dp(n+1,-1);
-        return fib(n,dp);
+         dp[0]=0;
+         dp[1]=1;
+
+        for(int i=2; i<=n; i++)
+        {
+            dp[i]= dp[i-1]+dp[i-2];
+           
+        }
+         return dp[n];
     }
 };
